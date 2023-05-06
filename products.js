47df0f16383421a6fbdb80edb3c99f7c943570c6
cesarrodriguez26 <124197857+cesarrodriguez26@users.noreply.github.com>
2023-05-06 13:55:11 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,26 @@
+const productos =[
+    {
+        id: 1,
+        nombre:"zapatillas adidas",
+        precio: 90,
+        img:"https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/e1dc6cc782f44c178643adf3001971ec_9366/Zapatilla_Forum_Low_Blanco_GY8556_011_hover_standard.jpg",
+    },
+    {
+        id: 2,
+        nombre:"zapatillas nike",
+        precio: 110,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/3587121c-5ff8-45b0-90b2-2cef3cb78b61/air-jordan-1-mid-zapatillas-8ZHMZq.png",
+    },
+    {
+        id: 3,
+        nombre:"zapatillas puma",
+        precio: 80,
+        img:"https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/389462/01/fnd/EEA/fmt/png/Zapatillas-Suede-Archive-Remastered",
+    },
+    {
+        id: 4,
+        nombre:"zapatillas lacoste",
+        precio: 120,
+        img:"https://image1.lacoste.com/dw/image/v2/AAQM_PRD/on/demandware.static/Sites-ES-Site/Sites-master/es/dw97cd52e0/43SMA0034_407_02.jpg?imwidth=915&impolicy=product",
+    },
+];
